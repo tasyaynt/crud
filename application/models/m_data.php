6651372b7efb56faf2ee119252dcedd7f1460188
tasyaynt <tasyaayunita84@gmail.com>
2022-10-04 16:11:15 +0700

    database

--- application/models/m_data.php
+++ application/models/m_data.php
@@ -5,10 +5,22 @@ class M_data extends CI_Model{
         $this->db->insert($table,$data);
         
     }
-
     function tampil_data(){
         return $this->db->get('mahasiswa');
     }
-}
 
+    function edit_data ($where,$table) {
+        return $this->db->get_where($table,$where);
+    }
+
+    function update_data($where,$data,$table) {
+        $this->db->where($where);
+        $this->db->update($table,$data);
+    }
+
+    function hapus_data($where,$table){
+        $this->db->where($where);
+        $this->db->delete($table);
+    }
+}
 ?> 
